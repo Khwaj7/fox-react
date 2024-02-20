7b06e31c4ab1a2ab51a0cd6359e1f32b3db2bc65
Philippe <borgodoff@gmail.com>
2024-02-20 11:46:28 +0100

    Fetch error handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import { useWar } from "./components/War/useWar";
 import { War } from "./modules/models/war";
 import Button from "@mui/joy/Button";
 import "@fontsource/inter";
-import ButtonGroup from '@mui/joy/ButtonGroup';
+import ButtonGroup from "@mui/joy/ButtonGroup";
+import { Snackbar, SnackbarOrigin } from "@mui/joy";
+
+interface SnackbarState extends SnackbarOrigin {
+  open: boolean;
+}
 
 function App() {
   const [selectedLive, setSelectedLive] = useState(1);
+  const [fetchError, setFetchError] = useState("");
   const [war, setWar] = useState({} as War);
 
-  const { getWar } = useWar({ setWar, selectedLive });
-  //getWar({ setWar });
+  const [open, setOpen] = useState(false);
+
+  const { getWar } = useWar({ setWar, selectedLive, setFetchError, setOpen });
 
   return (
     <>
@@ -20,8 +27,15 @@ function App() {
         <Button onClick={() => setSelectedLive(2)}>LIVE-2</Button>
         <Button onClick={() => setSelectedLive(3)}>LIVE-3</Button>
       </ButtonGroup>
-      <Button variant="solid" onClick={() => getWar()}>Get warID</Button>
+      <Button variant="solid" onClick={() => getWar()}>
+        Get warID
+      </Button>
       <p>{war.warId}</p>
+      {fetchError && (
+        <Snackbar color="danger" open={open}>
+          {fetchError}
+        </Snackbar>
+      )}
     </>
   );
 }
--- src/components/War/useWar.tsx
+++ src/components/War/useWar.tsx
@@ -4,17 +4,24 @@ import type { War } from "../../modules/models/war";
 interface IParams {
   setWar: (war: War) => void;
   selectedLive: number;
+  setFetchError: (error: string) => void;
+  setOpen: (open: boolean) => void;  
 }
 
 export function useWar(props: IParams) {
-  const { setWar, selectedLive } = props;
+  const { setWar, selectedLive, setFetchError, setOpen } = props;
 
   async function getWar(): Promise<void> {
     console.log(`selectedLive: ${selectedLive}`);
-    
-    const response = await fetchWar({ live:selectedLive });
 
-    setWar(response);
+    try {
+      const response = await fetchWar({ live: selectedLive });
+      setWar(response);
+    } catch (error) {
+      console.error(error);
+      setFetchError(`${error}`);
+      setOpen(true);
+    }
   }
 
   return { getWar };
